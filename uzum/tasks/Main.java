336b75e949c979b339962a769a489db93bdb20dd
Mirshakirov <mirsaid@Mirsaids-MacBook-Pro.local>
2025-08-27 13:21:32 +0500

    Added a Library task

--- uzum/tasks/Main.java
+++ uzum/tasks/Main.java
@@ -3,6 +3,7 @@ package uzum.tasks;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +14,30 @@ public class Main {
         System.out.println(Arrays.toString(main.twoSum(new int[]{1, 2, 5, 6, 7, 8}, 11)));
 
         System.out.println(Arrays.toString(main.twoPointers(new int[]{1, 1, 1, 2, 2, 3, 3, 3, 3})));
+
+        Library library = new Library();
+
+        Book firstBook = new Book("The Alchemist", "Paulo Coelho", 200, true);
+        Book secondBook = new Book("Clean Code", "Robert C. Martin", 450, false);
+        Book thirdBook = new Book("Effective Java", "Joshua Bloch", 380, true);
+
+        UUID firstId = library.add(firstBook);
+        UUID secondId = library.add(secondBook);
+        UUID thirdId = library.add(thirdBook);
+
+        System.out.println("\nAll books in library:");
+        library.getAll().forEach(System.out::println);
+
+        System.out.println("\nBook with ID " + secondId + ":");
+        System.out.println(library.getBookById(secondId));
+
+        System.out.println("\nRemoving book with ID " + firstId + "...");
+        library.remove(firstId);
+
+        System.out.println("\nAll books in library after removal:");
+        library.getAll().forEach(System.out::println);
     }
+
 
     public String wordCounter(String string, int num){
         HashMap<String, Integer> map = new HashMap<>();
